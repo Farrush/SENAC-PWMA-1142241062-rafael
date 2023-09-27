b22f7c0becc6f5ca5b505f1eeebdaebdb2833b2c
RAFAEL CARRENHO TARANTO <rafael.ctaranto@senacsp.edu.br>
2023-09-27 10:45:19 -0300

    Semana #07

--- PWMA-Semana#07/assets/js/script.js
+++ PWMA-Semana#07/assets/js/script.js
@@ -24,11 +24,57 @@ const produtos = [
         unidadesVendidas: 320
     }
 ]
+//Dados de venda total por mês, de jan 2022 a jan 2024
 const vendas = [
     {
         ano: 2022,
-        mes: 10,
+        mes: 1,
+        total: 32000
+    },
+    {
+        ano: 2022,
+        mes: 2,
+        total: 33000
+    },
+    {
+        ano: 2022,
+        mes: 3,
         total: 27000
+    },
+    {
+        ano: 2022,
+        mes: 4,
+        total: 26000
+    },
+    {
+        ano: 2022,
+        mes: 5,
+        total: 30000
+    },
+    {
+        ano: 2022,
+        mes: 6,
+        total: 28000
+    },
+    {
+        ano: 2022,
+        mes: 7,
+        total: 27000
+    },
+    {
+        ano: 2023,
+        mes: 8,
+        total: 25000
+    },
+    {
+        ano: 2022,
+        mes: 9,
+        total: 27000
+    },
+    {
+        ano: 2022,
+        mes: 10,
+        total: 26000
     },
     {
         ano: 2022,
@@ -106,8 +152,48 @@ const vendas = [
         total: 38000
     },
 ]
+
+function mediaCrescimentoMensal() {
+    // Array com as médias que será retornado
+    let crescimento = []
+    // Laço de repetição de 1 a 12 para representar meses
+    for (let i = 1; i <= 12; i++) {
+        // Em cada mês, reúne os dados de venda de cada mês em um array junto
+        let comparaMeses = vendas.filter(venda => venda.mes === i)
+        let media = 0
+        let quantidadePMedia = 0
+        //Laço que percorre cada objeto do comparaMeses
+        for (let e = 0; e < comparaMeses.length; e++) {
+            // Se o indice for 0, ele não vai fazer essa verificação de array[0] menos array[-1]
+            if (e - 1 >= 0) {
+                //Média recebe ela mesma + a diferença do valor atual e o do mesmo mês no ano anterior
+                media += comparaMeses[e].total - comparaMeses[e - 1].total
+                //Soma ao valor que a média será dividida
+                quantidadePMedia++
+            }
+        }
+        media = media / quantidadePMedia
+        //Adiciona no array de crescimento o objeto com os valores mês e média
+        crescimento.push({ mes: i, media: media })
+    }
+    return crescimento
+}
+
+const crescimentoMensal = mediaCrescimentoMensal()
+
+console.log("Array de Média de crescimento por mês:")
+console.log(crescimentoMensal)
+
+//construção do array que armazena a previsão de venda para os próximos 12 meses (Problema: Ele Lê o array de vendas inteiro e cria projeções para um mês que até já aconteceu)
 const previsaoVendas = vendas.map((venda, index) => {
-    return { ...venda, ano: venda.ano + 1, otimo: venda.total + 1500, regular: venda.total, ruim: venda.total - 1500 }
+    return {
+        // Cada objeto em previsaoVendas tem o próprio venda, com ano + 1, um valor de crescimento bom, um normal e um ruim.
+        ...venda,
+        ano: venda.ano + 1,
+        otimo: venda.total + crescimentoMensal[venda.mes - 1].media + 2000,// Essa média de crescimento vem de outro método
+        regular: venda.total + crescimentoMensal[venda.mes - 1].media + 200,
+        ruim: venda.total + crescimentoMensal[venda.mes - 1].media - 2000
+    }
 })
 
 if (!sessionStorage.getItem('user')) {
@@ -183,20 +269,20 @@ function loadVendas() {
     const linhaRegular = document.getElementById('linha-reg')
     const linhaRuim = document.getElementById('linha-ruim')
     vendas.map((venda, index) => {
-        if(index >= vendas.length - 12){
+        if (index >= vendas.length - 12) { // Exibe apenas os ultimos 12 meses, mas poderia mostrar tudo
             vendaAno.innerHTML += `<th>${venda.ano}</th>`
             colunasTabVendas.innerHTML += `<th>${getMes(venda.mes)}</th>`
             linhaTabVendas.innerHTML += `<td>${venda.total}</td>`
         }
-        
+
     })
     previsaoVendas.map((venda, index) => {
-        if(index >= vendas.length - 12){
-        projAno.innerHTML += `<th>${venda.ano}</th>`
-        mesesProj.innerHTML += `<th>${getMes(venda.mes)}</th>`
-        linhaOtimo.innerHTML += `<td>${venda.otimo}</td>`
-        linhaRegular.innerHTML += `<td>${venda.regular}</td>`
-        linhaRuim.innerHTML += `<td>${venda.ruim}</td>`
+        if (index >= vendas.length - 12) { // Exibe apenas os últimos 12 objetos de previsão de vendas, o que exibe o mês que vem e mais 11 meses
+            projAno.innerHTML += `<th>${venda.ano}</th>`
+            mesesProj.innerHTML += `<th>${getMes(venda.mes)}</th>`
+            linhaOtimo.innerHTML += `<td>${venda.otimo}</td>`
+            linhaRegular.innerHTML += `<td>${venda.regular}</td>`
+            linhaRuim.innerHTML += `<td>${venda.ruim}</td>`
         }
     })
 }
@@ -239,7 +325,7 @@ function getMes(numero) {
         case 12:
             mes = "Dezembro"
             break;
-        default: 
+        default:
             mes = "inválido"
     }
     return mes
